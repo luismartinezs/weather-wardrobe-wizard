5dddd194f0b5e7698bcdc840a491c39e9e523394
Luis Martinez <luismartinezwebdev@gmail.com>
2023-05-13 13:57:45 +0200

    style: tweak

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import {Container, Link} from "@chakra-ui/react";
 
 const Footer = (): JSX.Element => {
   return (
-      <Container color="gray.400" py={6}>
+      <Container color="gray.400" py={6} maxW="2xl">
         <small>
           Created by <Link fontWeight={800} href="https://www.luis-martinez.net/" isExternal>Luis Martinez</Link>
         </small>
