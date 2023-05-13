e1d7900172c45c06cd1d971b47fdc0e3031b3c41
Luis Martinez <luismartinezwebdev@gmail.com>
2023-05-13 14:34:43 +0200

    feat: scaffold middleware
    feat: abstract cors

--- src/pages/api/geocoding.ts
+++ src/pages/api/geocoding.ts
@@ -1,19 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+import cors, { runMiddleware } from '@/lib/cors'
 import { LocationSuggestion } from '@/types/weatherApi'
 
 const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY
 
-
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { query } = req.query
-  if (!query) {
-    res.status(400).json({ error: 'Please provide a location query' })
-    return
-  }
-
-  const url = `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=10&appid=${OPEN_WEATHER_API_KEY}`
   try {
+    await runMiddleware(req, res, cors)
+    const { query } = req.query
+    if (!query) {
+      res.status(400).json({ error: 'Please provide a location query' })
+      return
+    }
+
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=10&appid=${OPEN_WEATHER_API_KEY}`
     const response = await fetch(url)
     const data = (await response.json()).map((el: LocationSuggestion) => ({
       name: el.name,
--- src/pages/api/weather-forecast.ts
+++ src/pages/api/weather-forecast.ts
@@ -1,16 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+import cors, { runMiddleware } from '@/lib/cors'
+
 const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { lat, lon } = req.query
-  if (!lat || !lon) {
-    res.status(400).json({ error: 'Insufficient data to fetch a weather forecast' })
-    return
-  }
-
-  const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric&cnt=40`
   try {
+    await runMiddleware(req, res, cors)
+
+    const { lat, lon } = req.query
+
+    if (!lat || !lon) {
+      res.status(400).json({ error: 'Insufficient data to fetch a weather forecast' })
+      return
+    }
+
+    const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric&cnt=40`
     const response = await fetch(url)
     const data = await response.json()
 
