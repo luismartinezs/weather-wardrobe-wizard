d96d92b58adf488bb1e62ab2c312bd1b1162af43
Luis Martinez <luismartinezwebdev@gmail.com>
2023-05-14 07:23:44 +0200

    style: layout footer position

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,17 @@
-import { Box, Container } from "@chakra-ui/react";
+import { Box, Container, Stack } from "@chakra-ui/react";
 
 import Footer from "@/components/Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }): JSX.Element => {
   return (
-    <>
-      <Container maxW="container.xl" my={6}>
+    <Stack minH="100vh">
+      <Container maxW="container.xl" mt={6} mb={28}>
         {children}
       </Container>
-      <Box mt={10} bg="gray.900" >
+      <Box bg="gray.900" position='absolute' bottom={0} w="100%" >
         <Footer />
       </Box>
-    </>
+    </Stack>
   );
 };
 
