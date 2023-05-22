227f7d7c2db7f68972c1c34d5c85619894f5d48c
Luis Martinez <luismartinezwebdev@gmail.com>
2023-05-22 23:25:31 +0200

    chore: lists tests
    feat: setup playwright
    
    feat: tons of working tests
    
    feat: clothing suggestions test
    
    feat: clothing suggestions e2e tests

---
+++ tests/util/location.ts
@@ -0,0 +1,14 @@
+import { type Page, expect } from '@playwright/test';
+
+export async function setupLocation(page: Page) {
+  await page.goto('/');
+  await page.getByPlaceholder('Enter a location, e.g. Hanoi').click();
+  await page.getByPlaceholder('Enter a location, e.g. Hanoi').fill('hanoi');
+}
+
+export async function fetchForecast(page: Page) {
+  await setupLocation(page)
+  await page.getByRole('option', { name: 'Hanoi (VN)' }).click();
+  await page.getByRole('button', { name: 'Get weather in Hanoi' }).click();
+  await expect(page.locator('[data-testid="forecast-day"]:first-child')).toBeVisible();
+}
