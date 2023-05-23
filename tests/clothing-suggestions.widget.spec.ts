430c42d0f6a2fa75f92f0cfda342f9a436dcef16
Luis Martinez <luismartinezwebdev@gmail.com>
2023-05-23 09:55:30 +0200

    chore: try something

--- tests/clothing-suggestions.widget.spec.ts
+++ tests/clothing-suggestions.widget.spec.ts
@@ -1,6 +1,22 @@
-import { test, expect } from '@playwright/test';
+import {
+  test, expect,
+  type Page
+} from '@playwright/test';
 
-import { fetchForecast } from './util/location';
+// import { fetchForecast } from './util/location';
+
+export async function setupLocation(page: Page) {
+  await page.goto('/');
+  await page.getByPlaceholder('Enter a location, e.g. Hanoi').click();
+  await page.getByPlaceholder('Enter a location, e.g. Hanoi').fill('hanoi');
+}
+
+export async function fetchForecast(page: Page) {
+  await setupLocation(page)
+  await page.getByRole('option', { name: 'Hanoi (VN)' }).click();
+  await page.getByRole('button', { name: 'Get weather in Hanoi' }).click();
+  await expect(page.locator('[data-testid="forecast-day"]:first-child')).toBeVisible();
+}
 
 test.beforeEach(async ({ page }) => {
   await fetchForecast(page);
