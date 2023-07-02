7abbcd37d0397d0e81f8e18c349dfe31b5b84f5b
Luis Martinez <luismartinezwebdev@gmail.com>
2023-07-02 17:49:58 +0200

    chore: custom env e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig, devices } from '@playwright/test';
-import dotenv from 'dotenv';
+import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
+import path from "path";
 // import path from 'path';
 
 /**
@@ -8,13 +9,15 @@ import dotenv from 'dotenv';
  */
 // require('dotenv').config();
 
-dotenv.config();
+dotenv.config({
+  path: path.resolve(process.cwd(), ".env.test.local"),
+});
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e',
+  testDir: "./e2e",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -24,31 +27,34 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://weather-wardrobe-wizard.netlify.app/' : 'http://127.0.0.1:3000',
+    baseURL:
+      process.env.NODE_ENV === "production"
+        ? "https://weather-wardrobe-wizard.netlify.app/"
+        : "http://127.0.0.1:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
 
     /* Test against mobile viewports. */
--- src/firebase/hooks/useAppCheck.tsx
+++ src/firebase/hooks/useAppCheck.tsx
@@ -9,6 +9,12 @@ import { useEffect, useState } from "react";
 
 // const isProd = process.env.NODE_ENV === "production";
 
+declare global {
+  interface Window {
+    FIREBASE_APPCHECK_DEBUG_TOKEN?: string;
+  }
+}
+
 export function useAppCheck() {
   const [appCheck, setAppCheck] = useState<AppCheck | null>(null);
 
@@ -20,6 +26,14 @@ export function useAppCheck() {
     // if (!isProd) {
     //   window.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
     // }
+
+    if (process.env.NEXT_PUBLIC_E2E === "true") {
+      console.log("only e2e");
+      console.log(process.env.NEXT_PUBLIC_APP_CHECK_DEBUG_TOKEN_CI);
+      // Code to be run during Playwright testing here
+      window.FIREBASE_APPCHECK_DEBUG_TOKEN =
+        process.env.NEXT_PUBLIC_APP_CHECK_DEBUG_TOKEN_CI;
+    }
 
     setAppCheck(
       initializeAppCheck(app, {
