4cbea15bb5eef304eba1eea6f4799bca037cfa65
Luis Martinez <luismartinezwebdev@gmail.com>
2023-07-02 10:56:23 +0200

    refactor: clean up function imports

--- functions/src/utils/guards.ts
+++ functions/src/utils/guards.ts
@@ -1,44 +1,48 @@
-import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { CallableContext } from "firebase-functions/v1/https";
+import { HttpsError, CallableRequest } from "firebase-functions/v2/https";
 
 import { CUSTOMERS, SUBSCRIPTIONS } from "../constants";
 
-const handleGetCurrentUserRoles = async (auth: CallableContext["auth"]) => {
+const handleGetCurrentUserRoles = async (auth: CallableRequest["auth"]) => {
+  if (!auth) {
+    return [];
+  }
+
   const docRef = admin
     .firestore()
     .collection(CUSTOMERS)
-    .doc(auth!.uid)
+    .doc(auth.uid)
     .collection(SUBSCRIPTIONS);
   const subscriptions = await docRef.get();
+
   return subscriptions.docs.map((doc) => doc.data().role);
 };
 
 const isProd = process.env.NODE_ENV === "production";
 
-export const appCheckGuard = (context: CallableContext) => {
-  if (isProd && context.app == undefined) {
-    throw new functions.https.HttpsError(
+export const appCheckGuard = (request: CallableRequest) => {
+  if (isProd && request.app == undefined) {
+    throw new HttpsError(
       "failed-precondition",
       "The function must be called from an App Check verified app."
     );
   }
 };
 
-export const authGuard = (context: CallableContext) => {
-  if (!context.auth) {
-    throw new functions.https.HttpsError(
+export const authGuard = (request: CallableRequest) => {
+  if (!request.auth) {
+    throw new HttpsError(
       "failed-precondition",
       "The function must be called " + "while authenticated."
     );
   }
 };
 
-export const roleGuard = async (context: CallableContext, role: string) => {
-  const currentUserRoles = await handleGetCurrentUserRoles(context.auth);
+export const roleGuard = async (request: CallableRequest, role: string) => {
+  const currentUserRoles = await handleGetCurrentUserRoles(request.auth);
 
   if (!currentUserRoles.includes(role)) {
-    throw new functions.https.HttpsError(
+    throw new HttpsError(
       "failed-precondition",
       "The function must be called by a user with role " + role
     );
